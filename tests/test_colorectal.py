2cfc1f687fe91e4efbd0772215737e0b8bbc1f85
Zach Welshman <38403427+zwelshman@users.noreply.github.com>
2022-02-14 21:28:04 +0000

    Update test_colorectal.py

--- tests/test_colorectal.py
+++ tests/test_colorectal.py
@@ -8,20 +8,30 @@ import pandas as pd
 from functools import reduce
 
 
+
 # initialize sparksession
 spark = SparkSession.builder.appName('app').getOrCreate()
-# initialize list of lists
 
-data = [['Rand1', 'C34,C18,C19', '2021-01-01'], 
-        ['Rand1', 'C34,C18,C19', '2020-12-29'],
-        ['Rand3', 'C50', '2021-07-10'], 
-        ['Rand3', 'C50,C21,C50', '2021-04-01'], 
-        ['Rand3', 'J12, C20', '2021-06-01'],
-        ['Rand3', 'J12, C20', '2021-06-02'] ]
-# Create the pandas DataFrame
-pandasDF = pd.DataFrame(data, columns = ['dummy_name', 'dummy_condition','dummy_admis'])
+def test_df():
+        # initialize sparksession
+#        spark = SparkSession.builder.appName('app').getOrCreate()
+        # initialize list of lists
 
-#Create PySpark DataFrame from Pandas
-sparkDF=spark.createDataFrame(pandasDF) 
-sparkDF.printSchema()
-sparkDF.show()
+        data = [['Rand1', 'C34,C18,C19', '2021-01-01'], 
+                ['Rand1', 'C34,C18,C19', '2020-12-29'],
+                ['Rand3', 'C50', '2021-07-10'], 
+                ['Rand3', 'C50,C21,C50', '2021-04-01'], 
+                ['Rand3', 'J12, C20', '2021-06-01'],
+                ['Rand3', 'J12, C20', '2021-06-02'] ]
+        # Create the pandas DataFrame
+        pandasDF = pd.DataFrame(data, columns = ['dummy_name', 'dummy_condition','dummy_admis'])
+
+        #Create PySpark DataFrame from Pandas
+        sparkDF=spark.createDataFrame(pandasDF) 
+
+       
+        expected = sparkDF.collect()[0][0]
+        actual = [Row(10)]
+        
+        
+        assert actual == expected
